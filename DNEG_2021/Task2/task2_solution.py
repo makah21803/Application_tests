c4deb6c28e67c44ee9fb39ef3c270dae98099301
makah21803 <makah21803@gmail.com>
2021-08-22 15:41:03 +0200

    minor fix

--- DNEG_2021/Task2/task2_solution.py
+++ DNEG_2021/Task2/task2_solution.py
@@ -9,21 +9,20 @@ input_list = json.load(file)
 input_list = [int(i) for i in input_list]
 
 def solution(A):
-    def solution(A):
-        """
-        More efficient solution would be to count the same numbers after sort,
-        and just use combinatorics to get the result. That way we itterate just once for each number.
-        """
-        A.sort()
-        count = 0
-        for i, n in enumerate(A):
-            for j in range(i + 1, len(A)):
-                if A[i] == A[j]:
-                    count += 1
-                else:
-                    break
+    """
+    More efficient solution would be to count the same numbers after sort,
+    and just use combinatorics to get the result. That way we itterate just once for each number.
+    """
+    A.sort()
+    count = 0
+    for i, n in enumerate(A):
+        for j in range(i + 1, len(A)):
+            if A[i] == A[j]:
+                count += 1
+            else:
+                break
 
-        return count
+    return count
 
 
 print(solution(input_list))
